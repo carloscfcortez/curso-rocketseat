8a05e6beea8fc36e43fbdb1f1d89cd5ea19be091
Carlos Cortez <carlos.cfcortez@gmail.com>
2019-03-12 11:27:01 -0400

    alter api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://rocketseat-node.herokuapp.com/api"
+  //baseURL: "https://rocketseat-node.herokuapp.com/api"
+  baseURL: "https://api-lanches.herokuapp.com/api/v1"
 });
 
 export default api;
